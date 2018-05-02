a070b481e37237c6f187416835ecabea7c08a3a4
zy <18612332440@163.com>
2018-05-02 17:45:25 +0800

    商品放大组件

---
+++ pdShow.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="pdShow">
+        <div class="img-box">
+            <pic-zoom :url="url" :scale="3" :scroll="true"></pic-zoom>
+        </div>
+        <shop @url="getUrl" :url="shopUrl"></shop>
+    </div>
+</template>
+
+<script>
+    import PicZoom from './PicZoom'
+    import shop from './shop'
+
+    export default {
+        name: "pd-show",
+        props: {
+            shopUrl: {
+                type: Array,
+                default: false
+            }
+        },
+        data() {
+            return {
+                url: ''
+            }
+        },
+        components: {
+            PicZoom,
+            shop
+        },
+        methods: {
+            getUrl(url) {
+                this.url = url
+            }
+        },
+        mounted() {
+            this.url = this.shopUrl[0].url
+        }
+
+
+    }
+</script>
+
+<style lang="scss" scoped>
+    .land {
+        height: 500px;
+        width: 500px;
+    }
+
+    .img-box {
+        height: 400px;
+        width: 400px;
+    }
+</style>
---
+++ shop.vue
@@ -0,0 +1,57 @@
+<template>
+    <ul>
+        <li v-for="(item,index) in url" :class="[index==Index?'select':'']"><img :src=item.url alt=""
+                                                                                 @mouseover="change(item.url,index)">
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: "shop",
+        props: {
+            url: {
+                type: Array,
+                default: false
+            }
+        },
+        data() {
+            return {
+                Index: ''
+            }
+        },
+        methods: {
+            change(url, index) {
+                this.$emit('url', url);
+                this.Index = index
+                console.log(this.Index)
+            }
+        },
+
+        mounted() {
+            console.log(this.url)
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    ul {
+        width: 500px;
+        height: 50px;
+        background: red;
+        overflow: hidden;
+        li {
+            float: left;
+            width: 54px;
+            height: 54px;
+            border: 2px solid #fff;
+
+            img {
+                width: 50px;
+            }
+        }
+        .select {
+            border-color: #f40;;
+        }
+    }
+</style>
